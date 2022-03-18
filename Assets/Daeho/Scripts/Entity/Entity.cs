bb95c93cd2eda299a1b546e30f9dfdfa4483eb2a
daeho0818 <daeho0818@naver.com>
2022-03-18 16:09:43 +0900

    오크 보스 패턴 수정 중..
    수정 보스도 수정해야 함
    (수정 팔 길어질 때 대기시간 갖고 길어지기
    길어지는 속도 더 빠르게)

--- Assets/Daeho/Scripts/Entity/Entity.cs
+++ Assets/Daeho/Scripts/Entity/Entity.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public abstract class Entity : MonoBehaviour
 {
+    public float Hp { get; set; }
+    public bool IsDestroy => Hp == 0;
+
     [Tooltip("엔티티 콜라이더들 (1. 몸 | 2. 공격 1 | 3. 공격 2 | 4 공격 3 | ...)")]
     [SerializeField] protected Collider2D[] colliders;
+
     protected Rigidbody2D rigid;
+    protected SpriteRenderer renderer;
     protected abstract void Awake();
     protected abstract void Start();
     protected abstract void Update();
