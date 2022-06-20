51fbadec22dbc5e4ab7271c15d7ebef0abd5b03c
martin_newbie <g.martin.min@gmail.com>
2022-06-21 01:03:18 +0900

    d

--- Assets/Keunsub/Scripts/MarketPlace/Item/ItemOrcAxe.cs
+++ Assets/Keunsub/Scripts/MarketPlace/Item/ItemOrcAxe.cs
@@ -5,5 +5,33 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Item", menuName = "Item/OrcAxe", order = 0)]
 public class ItemOrcAxe : ItemBase
 {
+    public override void AtAttack(Entity monster)
+    {
+        throw new System.NotImplementedException();
+    }
 
+    public override void AtButtonClick()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void AtGameInit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void AtKill()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void AtOnDamage()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void AtUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
 }
