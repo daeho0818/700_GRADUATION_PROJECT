bb52c9b4eb5e2aa223b6b70ec6f37f617807fd7c
martin_newbie <g.martin.min@gmail.com>
2022-06-24 00:23:35 +0900

    add crystal waves

--- Assets/Keunsub/Scripts/InGame/Objects/Wave/CrystalWave.cs
+++ Assets/Keunsub/Scripts/InGame/Objects/Wave/CrystalWave.cs
@@ -38,7 +38,6 @@ public class CrystalWave : WaveBase
 
     IEnumerator Wave2Coroutine()
     {
-
         Platforms.Add(SpawnPlatform(new Vector2(-7f, -2f)));
         Platforms.Add(SpawnPlatform(new Vector2(-4.5f, -2f)));
         Platforms.Add(SpawnPlatform(new Vector2(7f, -2f)));
@@ -64,6 +63,20 @@ public class CrystalWave : WaveBase
 
     IEnumerator Wave3Coroutine()
     {
+        Monsters.Add(SpawnMonster(CubeCrystal, new Vector2(10f, -6.5f), EntitySize.Small));
+        Monsters.Add(SpawnMonster(CubeCrystal, new Vector2(-10f, -6.5f), EntitySize.Small));    
+
+        yield return StartCoroutine(WaitUntilMonsterDie(Monsters));
+        waveEnd = true;
+    }
+
+    void Wave4()
+    {
+        StartCoroutine(Wave4Coroutine());
+    }
+
+    IEnumerator Wave4Coroutine()
+    {
         Platforms.Add(SpawnPlatform(new Vector2(-10f, 1f)));
         Platforms.Add(SpawnPlatform(new Vector2(10f, 1f)));
 
@@ -81,38 +94,130 @@ public class CrystalWave : WaveBase
         waveEnd = true;
     }
 
-    void Wave4()
-    {
-
-    }
-
     void Wave5()
     {
+        StartCoroutine(Wave5Coroutine());
+    }
 
+    IEnumerator Wave5Coroutine()
+    {
+        Monsters.Add(SpawnMonster(WolfCrystal, new Vector2(10f, -6.5f), EntitySize.Big));
+
+        yield return StartCoroutine(WaitUntilMonsterDie(Monsters));
+        waveEnd = true;
     }
 
     void Wave6()
     {
+        StartCoroutine(Wave6Coroutine());
+    }
 
+    IEnumerator Wave6Coroutine()
+    {
+        Platforms.Add(SpawnPlatform(new Vector2(-3.75f, -3f)));
+        Platforms.Add(SpawnPlatform(new Vector2(-1.25f, -3f)));
+        Platforms.Add(SpawnPlatform(new Vector2(1.25f, -3f)));
+        Platforms.Add(SpawnPlatform(new Vector2(3.75f, -3f)));
+        Platforms.Add(SpawnPlatform(new Vector2(-3.75f, 0.5f)));
+        Platforms.Add(SpawnPlatform(new Vector2(-1.25f, 0.5f)));
+        Platforms.Add(SpawnPlatform(new Vector2(1.25f, 0.5f)));
+        Platforms.Add(SpawnPlatform(new Vector2(3.75f, 0.5f)));
+        Platforms.Add(SpawnPlatform(new Vector2(-3.75f, 4f)));
+        Platforms.Add(SpawnPlatform(new Vector2(-1.25f, 4f)));
+        Platforms.Add(SpawnPlatform(new Vector2(1.25f, 4f)));
+        Platforms.Add(SpawnPlatform(new Vector2(3.75f, 4f)));
+        yield return new WaitForSeconds(3f);
+
+        SpawnSharp();
+        yield return new WaitForSeconds(3f);
+
+        Monsters.Add(SpawnMonster(CubeCrystal, new Vector2(0, -1.2f), EntitySize.Small));
+
+        yield return StartCoroutine(WaitUntilMonsterDie(Monsters));
+        waveEnd = true;
     }
 
     void Wave7()
     {
+        StartCoroutine(Wave7Coroutine());
+    }
 
+    IEnumerator Wave7Coroutine()
+    {
+        Monsters.Add(SpawnMonster(CubeCrystal, new Vector2(0, 2.5f), EntitySize.Small));
+        Monsters.Add(SpawnMonster(MagicianCrystal, new Vector2(8f, 6f), EntitySize.Medium));
+        Monsters.Add(SpawnMonster(MagicianCrystal, new Vector2(-7.5f, -2.5f), EntitySize.Medium));
+
+        yield return StartCoroutine(WaitUntilMonsterDie(Monsters));
+        RemovePlatform(Platforms);
+        RemoveSharp();
+        waveEnd = true;
     }
 
     void Wave8()
     {
+        StartCoroutine(Wave8Coroutine());
+    }
 
+    IEnumerator Wave8Coroutine()
+    {
+        Platforms.Add(SpawnPlatform(new Vector2(0, -4f)));
+        Platforms.Add(SpawnPlatform(new Vector2(5.5f, -2.5f)));
+        Platforms.Add(SpawnPlatform(new Vector2(10.5f, 1.5f)));
+        Platforms.Add(SpawnPlatform(new Vector2(5.5f, 5.5f)));
+        Platforms.Add(SpawnPlatform(new Vector2(0, 2f)));
+        Platforms.Add(SpawnPlatform(new Vector2(-6.5f, 3f)));
+        Platforms.Add(SpawnPlatform(new Vector2(-10.5f, -2f)));
+
+        yield return new WaitForSeconds(5f);
+
+        Monsters.Add(SpawnMonster(MagicianCrystal, new Vector2(-4.5f, -1.5f), EntitySize.Medium));
+        yield return new WaitForSeconds(1f);
+        Monsters.Add(SpawnMonster(MagicianCrystal, new Vector2(8.6f, 3.5f), EntitySize.Medium));
+        yield return new WaitForSeconds(1f);
+        Monsters.Add(SpawnMonster(MagicianCrystal, new Vector2(-11f, 3f), EntitySize.Medium));
+        yield return new WaitForSeconds(1f);
+        Monsters.Add(SpawnMonster(MagicianCrystal, new Vector2(10f, -3.5f), EntitySize.Medium));
+        yield return new WaitForSeconds(1f);
+
+
+        yield return StartCoroutine(WaitUntilMonsterDie(Monsters));
+        waveEnd = true;
     }
 
     void Wave9()
     {
+        StartCoroutine(Wave9Coroutine());
+    }
 
+    IEnumerator Wave9Coroutine()
+    {
+        Monsters.Add(SpawnMonster(CubeCrystal, new Vector2(0, -2.5f), EntitySize.Small));
+        Monsters.Add(SpawnMonster(CubeCrystal, new Vector2(5.5f, -1f), EntitySize.Small));
+        Monsters.Add(SpawnMonster(CubeCrystal, new Vector2(10.5f, 3f), EntitySize.Small));
+        Monsters.Add(SpawnMonster(CubeCrystal, new Vector2(5.5f, 7f), EntitySize.Small));
+        Monsters.Add(SpawnMonster(CubeCrystal, new Vector2(0, 3.5f), EntitySize.Small));
+        Monsters.Add(SpawnMonster(CubeCrystal, new Vector2(-6.5f, 4.5f), EntitySize.Small));
+        Monsters.Add(SpawnMonster(CubeCrystal, new Vector2(-10.5f, -0.5f), EntitySize.Small));
+
+        yield return StartCoroutine(WaitUntilMonsterDie(Monsters));
+        
+        RemovePlatform(Platforms);
+        yield return new WaitForSeconds(3f);
+        waveEnd = true;
     }
 
     void Wave10()
     {
+        StartCoroutine(Wave10Coroutine());
+    }
 
+    IEnumerator Wave10Coroutine()
+    {
+        Monsters.Add(SpawnMonster(FistCrystal, new Vector2(-9.5f, -5f), EntitySize.Big));
+        Monsters.Add(SpawnMonster(WolfCrystal, new Vector2(10f, -5.5f), EntitySize.Big));
+
+        yield return StartCoroutine(WaitUntilMonsterDie(Monsters));
+        waveEnd = true;
     }
 }
